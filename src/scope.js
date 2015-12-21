87e305635edf35f355f5d0471e6a55d90e816824
Snger <sjs.stef@gmail.com>
2015-12-21 15:53:39 +0000

    fix(scope): reset $$lastDirtyWatch when a watch is add
    Fix bug that the new watch as the last dirty watch so the new watch is
    not being executed.

--- src/scope.js
+++ src/scope.js
@@ -19,6 +19,7 @@ Scope.prototype.$watch = function(watchFn, listenerFn) {
     last: initWatchVal
   };
   this.$$watchers.push(watcher);
+  this.$$lastDirtyWatch = null;
 };
 
 Scope.prototype.$$digestOnce = function() {
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -166,7 +166,27 @@ describe("[Angular-Scope]", function() {
       scope.array[0] = 666;
       scope.$digest();
       expect(watchExcutions).toBe(301);
-    })
+    });
+
+    it("does not end digest so that new watchers are not run", function(){
+      scope.aValue = 'abc';
+      scope.counter = 0;
+
+      scope.$watch(
+        function(scope){ return scope.aValue; },
+        function(newValue, oldValue, scope){
+          scope.$watch(
+            function(scope){ return scope.aValue; },
+            function(newValue, oldValue, scope){
+              scope.counter++;
+            }
+          )
+        }
+      );
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+    });
 
   });
 });
