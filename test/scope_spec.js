affcfa5ba0a68b1e145c02a48a1ca01c37a750cc
Snger <sjs.stef@gmail.com>
2015-12-21 21:41:47 +0000

    fix(scope): deal with NaN when equality check
    In JavaScript, NaN (Not-a-Number) is not equal to itself. This may
    sound strange, and that’s because it is. If we don’t explicitly
    handle NaN in our dirty-checking function, a watch that has NaN
    as a value will always be dirty.

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -208,5 +208,23 @@ describe("[Angular-Scope]", function() {
       expect(scope.counter).toBe(2);
     });
 
+    it("correctly handles NaN", function(){
+      scope.number = 0/0; // NaN
+      scope.counter = 0;
+
+      scope.$watch(
+        function(scope){ return scope.number; },
+        function(newValue, oldValue, scope){
+          scope.counter++;
+        }
+      );
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+      
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+    });
+
   });
 });
