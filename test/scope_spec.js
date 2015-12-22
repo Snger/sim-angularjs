4865186ef9f9fdbff7b687f5c1eeebe8ce860346
Snger <sjs.stef@gmail.com>
2015-12-22 22:50:58 +0000

    feat(scope): support scope phase
    Using $$phase to store the information about what's currently
    going on.

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -370,4 +370,59 @@ describe("[Angular-Scope]", function() {
     });
 
   });
+
+  describe("Scope Phases", function(){
+    var scope;
+
+    beforeEach(function(){
+      scope = new Scope();
+    });
+
+    it("has a $$phase field whose value is the current digest phase", function(){
+      scope.aValue = "someValue";
+      scope.phaseInWatchFunction = undefined;
+      scope.phaseInListenerFunction = undefined;
+      scope.phaseInApplyFunction = undefined;
+
+      scope.$watch(
+        function(scope){
+          scope.phaseInWatchFunction = scope.$$phase;
+          return scope.aValue;
+        },
+        function(newValue, oldValue, scope){
+          scope.phaseInListenerFunction = scope.$$phase;
+        }
+      );
+
+      scope.$apply(
+        function(scope){
+          scope.phaseInApplyFunction = scope.$$phase;
+        }
+      );
+      expect(scope.phaseInWatchFunction).toBe('$digest');
+      expect(scope.phaseInListenerFunction).toBe('$digest');
+      expect(scope.phaseInApplyFunction).toBe('$apply');
+    });
+
+    it("schedules a digest in $evalAsync", function(done){
+      scope.aValue = "someValue";
+      scope.counter = 0;
+
+      scope.$watch(
+        function(scope){ return scope.aValue; },
+        function(newValue, oldValue, scope){
+          scope.counter++;
+        }
+      );
+
+      scope.$evalAsync(function(scope){});
+
+      expect(scope.counter).toBe(0);
+      setTimeout(function () {
+        expect(scope.counter).toBe(1);
+        done();
+      }, 50);
+    });
+
+  });
 });
