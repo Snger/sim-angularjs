6ff24d5524eefdb57999d51d713ce68705073ebb
Snger <sjs.stef@gmail.com>
2015-12-22 20:05:05 +0000

    fix(scope): schedule $evalAsync from watch functions

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -315,5 +315,59 @@ describe("[Angular-Scope]", function() {
       expect(scope.asyncEvaluatedImmediately).toBe(false);
     });
 
+    it("executes $evalAsync'ed function added by watch function", function(){
+      scope.aValue = [1, 2, 3];
+      scope.asyncEvaluated = false;
+
+      scope.$watch(
+        function(scope){
+          if(!scope.asyncEvaluated){
+            scope.$evalAsync(function(scope){
+              scope.asyncEvaluated = true;
+            });
+          }
+          return scope.aValue;
+        },
+        function(newValue, oldValue, scope){}
+      );
+
+      scope.$digest();
+      expect(scope.asyncEvaluated).toBe(true);
+    });
+
+    it("executes $evalAsync'ed function even when not dirty", function(){
+      scope.aValue = [1, 2, 3];
+      scope.asyncEvaluatedTimes = 0;
+
+      scope.$watch(
+        function(scope){
+          if(scope.asyncEvaluatedTimes < 2){
+            scope.$evalAsync(function(scope){
+              scope.asyncEvaluatedTimes++;
+            });
+          }
+          return scope.aValue;
+        },
+        function(newValue, oldValue, scope){}
+      );
+
+      scope.$digest();
+      expect(scope.asyncEvaluatedTimes).toBe(2);
+    });
+
+    it("eventually halts $evalAsync added by watchers", function(){
+      scope.aValue = [1, 3, 4];
+
+      scope.$watch(
+        function(scope){
+          scope.$evalAsync(function(scope){ });
+          return scope.aValue;
+        },
+        function(newValue, oldValue, scope){}
+      );
+
+      expect(function(){scope.$digest();}).toThrow();
+    });
+
   });
 });
