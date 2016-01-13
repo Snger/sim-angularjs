9a33adc75c69ee9c5dca3032ff101c151cb411af
Snger <sjs.stef@gmail.com>
2016-01-13 21:51:00 +0000

    test(scope): allow a watch to destroy another during digest

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -279,6 +279,33 @@ describe("[Angular-Scope]", function() {
       expect(watchCalls).toEqual(['first', 'second', 'third', 'first', 'third']);
     });
 
+    it("allows a $watch to destroy another during digest", function(){
+      scope.aValue = 'abc';
+      scope.counter = 0;
+
+      scope.$watch(
+        function(scope){
+          return scope.aValue;
+        },
+        function(newValue, oldValue, scope){
+          destroyWatch();
+        }
+      );
+      var destroyWatch = scope.$watch(
+        function(scope){ },
+        function(newValue, oldValue, scope){ }
+      );
+      scope.$watch(
+        function(scope){ return scope.aValue; },
+        function(newValue, oldValue, scope){
+          scope.counter++;
+        }
+      );
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+    });
+
   });
 
   describe("$eval", function(){
